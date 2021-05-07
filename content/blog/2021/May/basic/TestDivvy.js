cfe5caefc8dc55794c5956a19b002ddbefed8308
Ted - TedfordMedia <ted@tedfordmedia.com>
2021-05-07 20:37:56 +0100

    ok

---
+++ content/blog/2021/May/basic/TestDivvy.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+ 
+
+// Static object
+const MyDivBox2 = styled.div({
+  background: 'pink',
+  height: '100px',
+  width: '100%',
+  padding: '1em'
+});
+// Static object
+ 
+
+export const Testbyyy = () => (
+  <div> 
+    <MyDivBox2>
+      hello MyDivBox2
+    </MyDivBox2>
+  </div>
+);
