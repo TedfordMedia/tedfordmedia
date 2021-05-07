f2b525f664d2529640b7b2ab88a2d8fdba654933
Ted <andy@sia365.com>
2021-05-07 21:21:16 +0100

    cube in canvas !

--- content/blog/2021/May/basic/TestDivvy.js
+++ content/blog/2021/May/basic/TestDivvy.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import { Canvas   } from '@react-three/fiber'
  
 
 // Static object
 const MyDivBox2 = styled.div({
   background: 'pink',
-  height: '100px',
+  height: '300px',
   width: '100%',
   padding: '1em'
 });
@@ -16,7 +16,15 @@ const MyDivBox2 = styled.div({
 export const Testbyyy = () => (
   <div> 
     <MyDivBox2>
-      hello MyDivBox2
+      hello MyDivBox2aa
+      <Canvas style={{ height: "100%", width: "100%" }}>
+      <mesh  >
+      <boxBufferGeometry args={[1, 1, 1]} attach="geometry" />
+      <meshPhongMaterial color={'red'} attach="material" />
+    </mesh> 
+      <directionalLight color="#ffffff" intensity={1} position={[-1, 2, 4]} />
+
+      </Canvas>
     </MyDivBox2>
   </div>
 );
