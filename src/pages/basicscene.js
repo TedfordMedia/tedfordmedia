1f8344943247a8380fe1141a4f196e3245ccba41
Ted - TedfordMedia <ted@tedfordmedia.com>
2021-05-08 20:09:53 +0100

    working redbox

--- src/pages/basicscene.js
+++ src/pages/basicscene.js
@@ -1,35 +1,12 @@
- 
-import * as THREE from 'three'
-import React, {   useRef, useMemo, Suspense} from 'react' 
-import { Canvas, useFrame  } from "@react-three/fiber"  
+import React from 'react' 
 import Layout from "../components/layoutwide" 
 import RobotStarsScene from "../components/basics/robots/robotscenea" 
-
-import {   OrbitControls   } from '@react-three/drei'
- 
  
 const IndexPage = (props) => (   
-  <Layout displayHero={false}>  
-
-    <section  >
-      <div  style={{ height: "600px", width: "100%",background:"yellow" }}> 
-    
-        <Canvas style={{ height: "500px", width: "100%" }} 
-          shadowMap
-          shadows
-          gl={{ alpha: false }}
-          camera={{ position: [-3, 1.1, 3], fov: 30 }}
-          onCreated={({ gl, camera, scene }) => { 
-            // gl.toneMapping = THREE.ACESFilmicToneMapping
-            // gl.outputEncoding = THREE.sRGBEncoding
-            gl.shadowMap.enabled = true;
-            gl.shadowMap.type = THREE.PCFSoftShadowMap;
-          }}>
-
-          <RobotStarsScene/> 
-          <OrbitControls target={[0,.8,0]}/>   
- 
-        </Canvas>
+  <Layout displayHero={false}>   
+    <section >
+      <div  style={{ height: "500px", width: "100%",background:"yellow" }}>  
+          <RobotStarsScene/>  
       </div>
     </section> 
   </Layout> 
