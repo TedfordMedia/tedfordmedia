3cd58a03d1919d0613f514856702aa7a741cfa05
Ted - TedfordMedia <ted@tedfordmedia.com>
2021-07-07 19:01:17 +0100

    cards

--- src/pages/indexmypage/index.js
+++ src/pages/indexmypage/index.js
@@ -3,23 +3,51 @@ import { graphql } from 'gatsby'
 import { Link } from "gatsby"
 import { Container, Row, Col, Card } from 'react-bootstrap'
 import Layout from "../../components/layoutwide"  
+import styled from '@emotion/styled'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './mystyling.css'
 import { useTheme } from '../../components/Theming'
-  
+import Image from 'gatsby-image'
+import { css } from '@emotion/core'
+const Description = styled.p`
+  margin-bottom: 10px;
+  font-size: .9em;
+  display: inline-block;
+`
+const StyledLink = styled(props => <Link {...props} />)`
+    color: black;
+    text-decoration: none;
+`;
+
 export default function Index({ data: { site, allMdx } }) {
   const theme = useTheme()
   return (
-    <Layout noSubscribeForm site={site}> 
+    <Layout noSubscribeForm site={site}>  
         {/* <Hero /> */}
         <Container>
             <Row>  
             {allMdx.edges.map(({ node: post }) => ( 
-                <Col key={post.id} lg={12}> 
-                    <Card> 
+                <Col key={post.id} lg={6}> 
+                    <Card className="mycardpart"> 
                         <Card.Body>
+                            {post.frontmatter.banner && (
+                                <Link
+                                    aria-label={`View ${post.frontmatter.title} article`}
+                                    to={`/${post.fields.slug}`}
+                                    >
+                                    <Image   
+                                        className="mycardimage"
+                                        sizes={post.frontmatter.banner.childImageSharp.fluid} 
+                                    /><br/>
+                                </Link>
+                            )}
                             <Card.Title>
-                                {post.frontmatter.title} 
+                                <StyledLink
+                                    to={'../'+post.frontmatter.slug}
+                                    aria-label={`View ${post.frontmatter.title}`}
+                                >
+                                    {post.frontmatter.title}
+                                </StyledLink>
                             </Card.Title>
                             <Card.Text>
                                 {post.excerpt}{' '}
@@ -67,8 +95,8 @@ export const pageQuery = graphql`
             description
             banner {
               childImageSharp {
-                sizes(maxWidth: 720) {
-                  ...GatsbyImageSharpSizes
+                fluid(maxWidth: 2000, quality: 100) {
+                    ...GatsbyImageSharpFluid
                 }
               }
             }
