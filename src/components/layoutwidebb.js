87dccdec23fb36ea381266f0082d52222ce907d2
Ted - TedfordMedia <ted@tedfordmedia.com>
2021-07-26 21:50:54 +0100

    z

--- src/components/layoutwidebb.js
+++ src/components/layoutwidebb.js
@@ -1,13 +1,23 @@
-import * as React from "react"    
-import { useTexture } from '@react-three/drei';
+import React , {  Suspense} from 'react';
+import { useTexture, Html } from '@react-three/drei';
 
 const LogoFloor = props => { 
   const myytexture = useTexture('./images/tedmedlogos/square_logo.png')   
   return (
-    <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
-        <planeBufferGeometry attach="geometry" args={[500, 500]} />
-        <meshStandardMaterial attach="material" map={myytexture} color="#999999" />
-    </mesh>
+    <Suspense fallback={<Html>Loading...</Html>}>    
+      <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
+          <planeBufferGeometry   attach="geometry" args={[50, 50]} />
+          <meshStandardMaterial   attach="material" map={myytexture}   />
+      </mesh>
+      <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[50, -1, 0]}>
+          <planeBufferGeometry  attach="geometry" args={[50, 50]} />
+          <meshStandardMaterial  attach="material" map={myytexture}  />
+      </mesh>
+      {/* <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[250, -1, 0]}>
+          <planeBufferGeometry attach="geometry" args={[100, 100]} />
+          <meshStandardMaterial attach="material" map={myytexture}  />
+      </mesh>  */}
+    </Suspense>
   )
 }
 
--- src/pages/followpath/index.js
+++ src/pages/followpath/index.js
@@ -28,26 +28,7 @@ const Torus = (props) => {
     </group>
   );
 };
-
-function TheShinyCube(){ 
-  const ref = useRef()   
-  const myytexture = useTexture('./images/tedmedlogos/square_logo_BW.png')  
- 
-  useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.002))
-
-  return (
-    <> 
-      <group position={[0, 0, 2]} ref={ref}> 
-        <mesh position={[-1, 0, 0]}>
-            <boxBufferGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial metalness={1} color={'#589bcd'} roughnessMap={myytexture} roughness={1}   /> 
-        </mesh> 
-      </group> 
-    </>
-  )
-}
- 
-
+  
 function TheNormalCube(){ 
   const ref = useRef()   
   const myytexture = useTexture('./images/tedmedlogos/square_logo.png')   
@@ -66,22 +47,32 @@ function TheNormalCube(){
   )
 }
  
-
+function Dolighting({ brightness, color }) {
+  return (
+    <group name="lighting">
+        <hemisphereLight intensity={.1} />
+        <ambientLight intensity={.1} />
+        {/* <pointLight castShadow position={[20, 80, 3]} intensity={.9}/>  */}
+ 
+        <pointLight castShadow 
+                shadow-mapSize-height={512}
+                shadow-mapSize-width={512}
+                position={[-10, 10, 20]} intensity={1}/>
+        {/* <directionalLight position={[67, 19, 127]}  intensity={.2} castShadow shadow-camera-zoom={2} />  */}
+        {/* <directionalLight position={[67,30,50]}  intensity={0.3} castShadow shadow-camera-zoom={2} /> */}
+       
+        {/* <directionalLight position={[67, 69, 7]}  intensity={.4} castShadow shadow-camera-zoom={2} />  */}
+        {/* <directionalLight position={[67,50,50]}  intensity={0.1} castShadow shadow-camera-zoom={2} /> */}
+        {/* <directionalLight position={[-57, 40,40]}  intensity={0.1} castShadow shadow-camera-zoom={2} />   */}
+    </group>
+  );
+}
+ 
 
 
 function TheCurveThing({children, scene}){
  
-  const [ref] = useRef() 
-
-//   const curve = new THREE.CatmullRomCurve3( [
-//     new THREE.Vector3( -4, 1, 9 ),
-//     new THREE.Vector3( -4, 1, 5 ),
-//     new THREE.Vector3( 0, 2, 0 ),
-//     new THREE.Vector3( 5, 1, 5 ),
-//     new THREE.Vector3( 5, 1, 8 ),
-//     new THREE.Vector3( -4, 1, 9 ),
-//   ] );
-  
+  const [ref] = useRef()  
 
     const curve = new THREE.CatmullRomCurve3( [
         new THREE.Vector3( -80, 0, - 80 ),
@@ -127,78 +118,72 @@ function TheCurveThing({children, scene}){
 const MyPage = (props) => (   
   <Layout displayHero={false}>    
       <div  style={{ height: "100vh", width: "100%",background:"#a0a0a0" }}>  
-        <Canvas camera={{ position: [-7, 3, 20], fov: 30 }} style={{ height: "100%", width: "100%" }}>
-            <ambientLight />
-
-            <LogoFloor/>
+        <Canvas  
+            shadowMap
+            shadows
+            gl={{ alpha: false }}
+            camera={{ 
+            position: [-3, 20.1, 80.5], 
+            fov: 30 ,  
+            near: 0.01,
+            far: 200
+            }}
+            onCreated={({ gl, camera, scene }) => {  
+                gl.outputEncoding = THREE.sRGBEncoding
+                gl.shadowMap.enabled = true;
+                gl.shadowMap.type = THREE.PCFSoftShadowMap; 
+                // const fogColor = new THREE.Color(0x00ff00);
+                // scene.fog = new THREE.Fog(fogColor, 9.0025, 80);
+                scene.background = new THREE.Color( 0xa0a0a0 );
+            }}
+            style={{ height: "100%", width: "100%" }}
+        >
  
-          
-          <pointLight position={[-10, 20, 20]} intensity={1}/>
-          <pointLight position={[3, 3, 3]} intensity={1}/>
-          {/* <Stars/> */}
+            <Dolighting/>
 
-
-
-            <directionalLight position={[67, 19, 127]}  intensity={.2} castShadow shadow-camera-zoom={2} /> 
-            <directionalLight position={[67,30,50]}  intensity={0.3} castShadow shadow-camera-zoom={2} />
-            <directionalLight position={[-57, 30,40]}  intensity={0.3} castShadow shadow-camera-zoom={2} />
-
-            <mesh castShadow receiveShadow position={[0, -.5, 0]} >
+            <Suspense fallback={<Html>Loading...</Html>}>  
+                <LogoFloor/>
+            </Suspense>
+            
+            <mesh castShadow receiveShadow position={[-5, -.5, 0]} >
                 <boxBufferGeometry args={[2, 2, 2]}  attach="geometry" />
-                <meshPhongMaterial color={'blue'} attach="material" />
+                <meshPhongMaterial color={'#4e7ea4'} attach="material" />
             </mesh> 
- 
-
-
-
-
-
-          <TheCurveThing> 
-          </TheCurveThing> 
+            <mesh castShadow receiveShadow position={[25, -.5, -30]} >
+                <boxBufferGeometry args={[100, 10, 10]}  attach="geometry" />
+                <meshPhongMaterial color={'#4e7ea4'} attach="material" />
+            </mesh> 
+            <TheCurveThing/>  
 
             <Suspense fallback={<Html>Loading...</Html>}>    
-            <group position={[20, 16, 10]} scale={[.025,.025,.025]}>
-               <Flamingo  /> 
-            </group>
-          </Suspense>
-          <Suspense fallback={<Html>Loading...</Html>}>    
-            <group position={[0, 1, -2]} scale={[.02,.02,.02]}>
-               <Flamingo  /> 
-            </group>
-          </Suspense>
-
-
-            <Suspense fallback={<Html>Loading...</Html>}>    
-                <group position={[10, 10, 0]} scale={[1.02,1.02,1.02]}>
-                {/* <Box  />  */}
+                <group position={[20, 16, 10]} scale={[.025,.025,.025]}>
+                <Flamingo  /> 
                 </group>
             </Suspense>
- 
-            <Suspense fallback={<Html><h1 style={{color:'white'}}>Loading...</h1></Html>}>  
-                <TheShinyCube/>   
+
+            <Suspense fallback={<Html>Loading...</Html>}>    
+                <group position={[0, 1, -2]} scale={[.02,.02,.02]}>
+                <Flamingo  /> 
+                </group>
             </Suspense> 
 
-      <Suspense fallback={<Html><h1 style={{color:'white'}}>Loading...</h1></Html>}>  
-     <group position={[3, 8, 0]} scale={[1.02,1.02,1.02]}>
-                <TheNormalCube/>   
-        </group>
+            <Suspense fallback={<Html><h1 style={{color:'white'}}>Loading...</h1></Html>}>  
+                <group position={[3, 8, 0]} scale={[1.02,1.02,1.02]}>
+                    <TheNormalCube/>   
+                </group>
             </Suspense> 
 
             <group position={[3, 3, 0]} scale={[1.02,1.02,1.02]}>
                 <Torus />
             </group>
-   
-          <OrbitControls/>
+           
+            <OrbitControls  maxDistance={50} maxPolarAngle={Math.PI / 2}/>
         </Canvas>
       </div> 
   </Layout> 
 )
  
-
-	// scene.background = new THREE.Color( 0xa0a0a0 );
-	// scene.fog = new THREE.Fog( 0xa0a0a0,100	, 5000 );
-
-
-
-
+// scene.background = new THREE.Color( 0xa0a0a0 );
+// scene.fog = new THREE.Fog( 0xa0a0a0,100	, 5000 );
+ 
 export default MyPage  
