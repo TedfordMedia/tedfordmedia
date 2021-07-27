45b6a4477d4e899dc546b723dbccbce851c9dd05
Ted - TedfordMedia <ted@tedfordmedia.com>
2021-07-27 01:04:45 +0100

    z

--- src/components/layoutwidebb.js
+++ src/components/layoutwidebb.js
@@ -5,14 +5,14 @@ const LogoFloor = props => {
   const myytexture = useTexture('./images/tedmedlogos/square_logo.png')   
   return (
     <Suspense fallback={<Html>Loading...</Html>}>    
-      <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
-          <planeBufferGeometry   attach="geometry" args={[50, 50]} />
+      <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.9, 5]}>
+          <planeBufferGeometry   attach="geometry" args={[20, 20]} />
           <meshStandardMaterial   attach="material" map={myytexture}   />
       </mesh>
-      <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[50, -1, 0]}>
+      {/* <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[50, -1, 0]}>
           <planeBufferGeometry  attach="geometry" args={[50, 50]} />
           <meshStandardMaterial  attach="material" map={myytexture}  />
-      </mesh>
+      </mesh> */}
       {/* <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[250, -1, 0]}>
           <planeBufferGeometry attach="geometry" args={[100, 100]} />
           <meshStandardMaterial attach="material" map={myytexture}  />
