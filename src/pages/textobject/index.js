270d9ddd01246ad3bb8366319b2eec5bf0181b3a
Ted - TedfordMedia <ted@tedfordmedia.com>
2021-07-28 15:11:28 +0100

    z

--- src/pages/textobject/index.js
+++ src/pages/textobject/index.js
@@ -1,31 +1,60 @@
 import * as THREE from 'three'
 import React, { Suspense, useRef, useMemo } from 'react'
 import { Canvas, extend, useThree, useLoader, useFrame } from '@react-three/fiber'
-import { OrbitControls, Sky } from '@react-three/drei'
+import { Text , OrbitControls} from '@react-three/drei'
 import { Water } from 'three-stdlib'
 import { css } from '@emotion/core'
 import Layout from "../../components/layoutwidellh"   
-
-extend({ Water })
-
+import Roboto from '/static/helvetiker_regular.typeface.json';
  
-function Box() {
-  const ref = useRef()
-  useFrame((state, delta) => {
-    ref.current.position.y = 10 + Math.sin(state.clock.elapsedTime) * 20
-    ref.current.rotation.x = ref.current.rotation.y = ref.current.rotation.z += delta
-  })
-  return (
-    <mesh ref={ref} scale={20}>
-      <boxGeometry />
-      <meshStandardMaterial />
-    </mesh>
+const DISPLAY_TEXT = 'TedfordMedia';
+ 
+
+function MyThing2({ clicked, ...props }) { 
+   const font = new THREE.FontLoader().parse(Roboto);
+
+ const textOptions = {
+    font,
+    size: 5,
+    height: 1
+  };
+  // const Myfont = useLoader(
+  //   THREE.FontLoader,
+  //   'https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/fonts/helvetiker_regular.typeface.json'
+  // );
+
+//  const Myfont = useLoader(
+//     THREE.FontLoader,
+//     './Inter-Bold.woff'
+//   );
+  // useFrame((state, delta) => (ref.current.material.uniforms.time.value += delta))
+    return ( 
+
+   <mesh>
+        <textGeometry attach='geometry' args={['three.js', textOptions]} />
+        <meshStandardMaterial attach='material' />
+      </mesh>
+
+  )
+}
+function MyThding2({ clicked, ...props }) {  
+    return (
+       <Text font="/Inther-Bold.woff" fontSize={3} letterSpacing={-0.06} {...props}>
+      Drei 2d text I am
+      <meshBasicMaterial color="green" > 
+      </meshBasicMaterial>
+    </Text> 
   )
 }
 
+const MyPage = (props) => {
+  const DISPLAY_TEXT = 'TedfordMedia';
 
-const MyPage = (props) => (   
- <Layout  css={css`  
+ 
+
+
+  return(
+    <Layout  css={css`  
       `}>    
       <div  className={'mydiv'}       
         css={css` 
@@ -35,18 +64,18 @@ const MyPage = (props) => (
         <Canvas 
             style={{ height: "100%", width: "100%" }}
             camera={{ position: [0, 5, 100], fov: 55, near: 1, far: 20000 }}>
-            <pointLight position={[100, 100, 100]} />
-            <pointLight position={[-100, -100, -100]} />
-            <Suspense fallback={null}>
-              
-                <Box />
-            </Suspense>
-            <Sky scale={1000} sunPosition={[500, 150, -1000]} turbidity={0.1} />
-            <OrbitControls />
+          <Suspense fallback={null}>
+            <MyThing2/>            
+          </Suspense>
+ 
+          <Suspense fallback={null}>
+            <MyThding2/>            
+          </Suspense> 
+
+          <OrbitControls  maxDistance={650} maxPolarAngle={Math.PI / 2}/>
         </Canvas>
-      </div>
-  
+      </div> 
   </Layout> 
-)
+)}
   
 export default MyPage  
