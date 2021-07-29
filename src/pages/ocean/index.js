e99c50595cc9ffc28af783a5cf193856909e96a0
Ted - TedfordMedia <ted@tedfordmedia.com>
2021-07-29 23:21:05 +0100

    z

--- src/pages/ocean/index.js
+++ src/pages/ocean/index.js
@@ -1,11 +1,11 @@
 import * as THREE from 'three'
 import React, { Suspense, useRef, useMemo } from 'react'
 import { Canvas, extend, useThree, useLoader, useFrame } from '@react-three/fiber'
-import { OrbitControls, Sky, useTexture } from '@react-three/drei'
+import { OrbitControls, Sky, useTexture, Html } from '@react-three/drei'
 import { Water } from 'three-stdlib'
 import { css } from '@emotion/core'
 
-
+import Sub from "../../helpers/Steampunk_submarine"; 
  
  import Layout from "../../components/layoutwidellh"  
 // import { Canvas, useFrame} from "@react-three/fiber"
@@ -45,12 +45,12 @@ function Box() {
   const myytexture = useTexture('./images/tedmedlogos/square_logo.png')   
  
   const ref = useRef()
-  useFrame((state, delta) => {
-    ref.current.position.y = 10 + Math.sin(state.clock.elapsedTime) * 20
-    ref.current.rotation.x = ref.current.rotation.y = ref.current.rotation.z += delta
-  })
+  // useFrame((state, delta) => {
+  //   ref.current.position.y = 10 + Math.sin(state.clock.elapsedTime) * 20
+  //   ref.current.rotation.x = ref.current.rotation.y = ref.current.rotation.z += delta
+  // })
   return (
-    <mesh ref={ref} scale={20}>
+    <mesh position={[63,10,0]} ref={ref} scale={30}>
       <boxGeometry />
       <meshStandardMaterial  color={"white"} map={myytexture} />
     </mesh>
@@ -67,16 +67,23 @@ const MyPage = (props) => (
         overflow: hidden;
       `}>  
         <Canvas 
-            style={{ height: "100%", width: "100%" }}
-            camera={{ position: [0, 5, 100], fov: 55, near: 1, far: 20000 }}>
+            style={{ height: "100%", width: "100%" }}  lookAt={[0, -10, 2]} 
+            camera={{ position: [0, 35, 150], fov: 55, near: 1, far: 20000 }}>
             <pointLight position={[100, 100, 100]} />
             <pointLight position={[-100, -100, -100]} />
-            <Suspense fallback={null}>
+          <ambientLight intensity={.5} />
+
+            <Suspense fallback={<Html><h1 style={{color:'blue'}}>Loading...</h1></Html>}>  
                 <Ocean />
-                <Box />
+                <Box /> 
             </Suspense>
+
+            <Suspense fallback={<Html><h1 style={{color:'white'}}>Loading...</h1></Html>}>  
+              <Sub/>
+            </Suspense>
+
             <Sky scale={1000} sunPosition={[500, 150, -1000]} turbidity={0.1} />
-            <OrbitControls />
+            <OrbitControls  maxDistance={650} maxPolarAngle={Math.PI / 2} autoRotate autoRotateSpeed={-.8}/>
         </Canvas>
       </div>
   
