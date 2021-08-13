4c6de4a714e70245fe24a2d83572a40b170b34dd
Ted - TedfordMedia <ted@tedfordmedia.com>
2021-08-13 22:14:07 +0100

    facefilter

---
+++ src/pages/facetracking/index.js
@@ -0,0 +1,40 @@
+// import React, { useState, useEffect, useRef } from 'react'
+// import { Canvas, useFrame, useThree } from 'react-three-fiber'
+ 
+import styled from '@emotion/styled'
+// // import 'bootstrap/dist/css/bootstrap.min.css'
+import '../mystyling.scss'
+// // import main script and neural network model from Jeeliz FaceFilter NPM package
+import { JEELIZFACEFILTER, NN_4EXPR } from 'facefilter'
+
+// // import THREE.js helper, useful to compute pose 
+import { JeelizThreeFiberHelper } from '../contrib/faceFilter/JeelizThreeFiberHelper.js'
+
+
+ 
+
+import * as THREE from 'three'
+import React, { Suspense, useRef, useMemo , useState,useEffect} from 'react'
+import { Canvas, extend, useThree, useLoader, useFrame } from '@react-three/fiber'
+import { OrbitControls, Sky, useTexture, Html } from '@react-three/drei' 
+import { css } from '@emotion/core'
+ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+ import Layout from "../../components/layoutwidellh"   
+import Robot from "../../helpers/Robot6dance.js";
+ 
+
+const MyPage = (props) => (   
+ <Layout  css={css`  
+      `}>    
+      <div  className={'mydiv'}       
+        css={css` 
+        height: 100%;
+        overflow: hidden;
+      `}>  
+        i am canvas
+      </div>
+  
+  </Layout> 
+)
+  
+export default MyPage  
