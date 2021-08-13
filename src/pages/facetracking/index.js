a0a7baf9f0084ff8111ee0fb1a28e9dc655d29f8
Ted - TedfordMedia <ted@tedfordmedia.com>
2021-08-13 23:59:12 +0100

    face tracking

--- src/pages/facetracking/index.js
+++ src/pages/facetracking/index.js
@@ -5,37 +5,43 @@ import styled from '@emotion/styled'
 // // import 'bootstrap/dist/css/bootstrap.min.css'
 // import '../mystyling.scss'
 // // // import main script and neural network model from Jeeliz FaceFilter NPM package
- 
-
+  
 import * as THREE from 'three'
 import React, { Suspense, useRef, useMemo , useState,useEffect} from 'react'
 import { Canvas, extend, useThree, useLoader, useFrame } from '@react-three/fiber'
 import { OrbitControls, Sky, useTexture, Html } from '@react-three/drei' 
 import { css } from '@emotion/core'
- import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
- import Layout from "../../components/layoutwidellh"   
-
-
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import Layout from "../../components/layoutwidellh"   
  
-
 // import main script and neural network model from Jeeliz FaceFilter NPM package
 import { JEELIZFACEFILTER, NN_4EXPR } from 'facefilter'
 
 // import THREE.js helper, useful to compute pose 
 import { JeelizThreeFiberHelper } from './JeelizThreeFiberHelper.js'
-
-
-
+ 
 const _maxFacesDetected = 1 // max number of detected faces
 const _faceFollowers = new Array(_maxFacesDetected)
 let _expressions = null
 
+function Box() {
+  const myytexture = useTexture('./images/tedmedlogos/square_logo_flip.png')   
+ 
+  const ref = useRef() 
+  return (
+    <mesh position={[0,0,0]} ref={ref} scale={1}>
+      <boxGeometry />
+      <meshBasicMaterial  color={"white"} map={myytexture} />
+    </mesh>
+  )
+}
 
 // This mesh follows the face. put stuffs in it.
 // Its position and orientation is controlled by Jeeliz THREE.js helper
 const FaceFollower = (props) => {
   // This reference will give us direct access to the mesh
   const objRef = useRef()
+
   useEffect(() => {
     const threeObject3D = objRef.current
     _faceFollowers[props.faceIndex] = threeObject3D  
@@ -43,6 +49,7 @@ const FaceFollower = (props) => {
   
   const mouthOpenRef = useRef()
   const mouthSmileRef = useRef()
+
   useFrame(() => {
     if (mouthOpenRef.current){
       const s0 = props.expression.mouthOpen
@@ -57,11 +64,10 @@ const FaceFollower = (props) => {
 
   return (
     <object3D ref={objRef}>
-      <mesh name="mainCube">
-        <boxBufferGeometry args={[1, 1, 1]} />
-        <meshNormalMaterial />
-      </mesh>
-
+      <Suspense fallback={<Html><h1 style={{color:'blue'}}>Loading...</h1></Html>}>   
+          <Box /> 
+      </Suspense>
+ 
       <mesh ref={mouthOpenRef} rotation={[Math.PI/2,0,0]} position={[0, -0.2, 0.2]}>
         <cylinderGeometry args={[0.3,0.3, 1, 32]} />
         <meshBasicMaterial color={0xff0000} />
@@ -74,8 +80,7 @@ const FaceFollower = (props) => {
     </object3D>
   )
 }
-
-
+ 
 // fake component, display nothing
 // just used to get the Camera and the renderer used by React-fiber:
 let _threeFiber = null
@@ -99,9 +104,130 @@ const compute_sizing = () => {
 }
 
  
+const MyPage = (props) => {
+
+  // init state:
+  _expressions = []
+  for (let i = 0; i<_maxFacesDetected; ++i){
+    _expressions.push({
+      mouthOpen: 0,
+      mouthSmile: 0,
+      eyebrowFrown: 0,
+      eyebrowRaised: 0
+    })
+  }  
+  const [sizing, setSizing] = useState(compute_sizing())
+  const [isInitialized] = useState(true)
+
+  let _timerResize = null
+  const handle_resize = () => {
+    // do not resize too often:
+    if (_timerResize){
+      clearTimeout(_timerResize)
+    }
+    _timerResize = setTimeout(do_resize, 200)
+  }
+ 
+  const do_resize = () => {
+    _timerResize = null
+    const newSizing = compute_sizing()
+    setSizing(newSizing)
+  }
+ 
+  useEffect(() => {
+    if (!_timerResize) {
+      JEELIZFACEFILTER.resize()
+    }    
+  }, [sizing])
+ 
+  const callbackReady = (errCode, spec) => {
+    if (errCode){
+      console.log('AN ERROR HAPPENS. ERR =', errCode)
+      return
+    }
+
+    console.log('INFO: JEELIZFACEFILTER IS READY')
+    // there is only 1 face to track, so 1 face follower:
+    JeelizThreeFiberHelper.init(spec, _faceFollowers, callbackDetect)    
+  }
+ 
+  const callbackTrack = (detectStatesArg) => {
+    // if 1 face detection, wrap in an array:
+    const detectStates = (detectStatesArg.length) ? detectStatesArg : [detectStatesArg]
+
+    // update video and THREE faceFollowers poses:
+    JeelizThreeFiberHelper.update(detectStates, _threeFiber.camera)
+
+    // render the video texture on the faceFilter canvas:
+    JEELIZFACEFILTER.render_video()
+
+    // get expressions factors:
+    detectStates.forEach((detectState, faceIndex) => {
+      const exprIn = detectState.expressions
+      const expression = _expressions[faceIndex]
+      expression.mouthOpen = exprIn[0]
+      expression.mouthSmile = exprIn[1]
+      expression.eyebrowFrown = exprIn[2] // not used here
+      expression.eyebrowRaised = exprIn[3] // not used here
+    })    
+  }
+ 
+  const callbackDetect = (faceIndex, isDetected) => {
+    if (isDetected) {
+      console.log('DETECTED')
+    } else {
+      console.log('LOST')
+    }
+  }
+
+  const faceFilterCanvasRef = useRef(null)
+  useEffect(() => {
+    window.addEventListener('resize', handle_resize)
+    window.addEventListener('orientationchange', handle_resize)
+
+    JEELIZFACEFILTER.init({
+      canvas: faceFilterCanvasRef.current,
+      NNC: NN_4EXPR,
+      maxFacesDetected: 1,
+      followZRot: true,
+      callbackReady,
+      callbackTrack
+    })
+    return JEELIZFACEFILTER.destroy
+  }, [isInitialized])
+
+  console.log('RENDER')
+
+  return (
+    <div>
+      {/* Canvas managed by three fiber, for AR: */}
+      <Canvas className='mirrorX' style={{
+        position: 'fixed',
+        zIndex: 2,
+        ...sizing
+      }}
+      gl={{
+        preserveDrawingBuffer: true // allow image capture
+      }}
+      updateDefaultCamera = {false}
+      >
+        <DirtyHook sizing={sizing} />
+        <FaceFollower faceIndex={0} expression={_expressions[0]} />
+      </Canvas>
+
+    {/* Canvas managed by FaceFilter, just displaying the video (and used for WebGL computations) */}
+      <canvas className='mirrorX' ref={faceFilterCanvasRef} style={{
+        position: 'fixed',
+        zIndex: 1,
+        ...sizing
+      }} width = {sizing.width} height = {sizing.height} />
+    </div>
+  )  
+} 
+
  
 
-const MyPage = (props) => {
+const xMyPage = (props) => {
 
 console.log('hello')
 
