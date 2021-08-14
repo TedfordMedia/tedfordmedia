22a8c3e0235295ea08ca1fe0d0db28a70a8f021b
Ted - TedfordMedia <ted@tedfordmedia.com>
2021-08-14 02:36:44 +0100

    face track

--- src/pages/facetracking/index.js
+++ src/pages/facetracking/index.js
@@ -1,15 +1,10 @@
-// import React, { useState, useEffect, useRef } from 'react'
-// import { Canvas, useFrame, useThree } from 'react-three-fiber'
+
+import styled from '@emotion/styled'  
  
-import styled from '@emotion/styled'
-// // import 'bootstrap/dist/css/bootstrap.min.css'
-// import '../mystyling.scss'
-// // // import main script and neural network model from Jeeliz FaceFilter NPM package
-  
 import * as THREE from 'three'
 import React, { Suspense, useRef, useMemo , useState,useEffect} from 'react'
-import { Canvas, extend, useThree, useLoader, useFrame } from '@react-three/fiber'
-import { OrbitControls, Sky, useTexture, Html } from '@react-three/drei' 
+import { Canvas, useThree, useLoader, useFrame } from '@react-three/fiber'
+import { OrbitControls, useTexture, Html } from '@react-three/drei' 
 import { css } from '@emotion/core'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import Layout from "../../components/layoutwidellh"   
