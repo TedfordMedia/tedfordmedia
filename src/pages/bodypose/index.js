6be72954f8b47a6677a169cd94187ba6ebeae8a9
Ted - TedfordMedia <ted@tedfordmedia.com>
2021-08-17 03:02:07 +0100

    z

---
+++ src/pages/bodypose/index.js
@@ -0,0 +1,19 @@
+import * as handpose from "@tensorflow-models/handpose";
+import Webcam from "react-webcam"; 
+import React, { Suspense,  useEffect, useCallback, useState } from 'react'
+import { Canvas, useTexture, useFrame } from '@react-three/fiber' 
+ 
+import Layout from "../../components/layoutwidellh"  
+ 
+function MyPage(props){
+ 
+  
+ 
+  return (
+    <Layout>    
+         
+<div>asd</div>
+    </Layout> 
+)}
+  
+export default MyPage  
